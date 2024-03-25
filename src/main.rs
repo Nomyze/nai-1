478a86fc9d22fb08c0eb94c73368eefbe23fff3c
Szymon Kościelski <nomyze@proton.me>
2024-03-25 17:11:03 +0100

    Perceptron ver.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 use std::io;
-use std::collections::HashMap;
 use std::io::Write;
-
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 #[derive(Clone)]
 struct Rekord {
@@ -10,44 +10,53 @@ struct Rekord {
     class: String,
 }
 
-#[derive(Clone)]
-struct TrainData {
-    records: Vec<Rekord>,
+struct Perceptron {
+    weights: Vec<f64>,
+    theta: f64,
 }
 
 fn main() {
-    let train_data = from_file("iris_training.txt");
-    println!("Displaying read data:");
-    train_data.records.clone().into_iter().for_each(|rek| {
-        println!("{}", rek.to_string());
-    });
     let mut buffer = String::new();
     let stdin = io::stdin();
-    print!("k: ");
+    print!("alpha: ");
     std::io::stdout().flush().unwrap();
     let _ = stdin.read_line(&mut buffer);
-    let k = buffer.trim().parse::<i32>().expect("Failed to convert to an integer");
-    let test = from_file("iris_test.txt");
+    let k = buffer.trim().parse::<f64>().expect("Failed to convert to a float");
+    let perceptron = from_file("iris_training.txt", k);
+    println!("Displaying read data:");
+    println!("w: {:?} t: {}", perceptron.weights, perceptron.theta);
+    //let test = from_file("iris_test.txt");
+    let contents = fs::read_to_string("iris_test.txt")
+        .expect("Failed to read file");
+    let mut records: Vec<Rekord> = Vec::new();
+    for line in contents.lines() {
+        records.push(line_to_rekord(line, false));
+    }
+    
     let mut count = 0;
     let mut hit = 0;
-    for rek in test.records {
-        let res = train_data.classify(k, &rek.attribs);
+    for rek in records {
+        //let res = train_data.classify(k, &rek.attribs);
+        let out = perceptron.get_output(&rek);
+        let res = if out == 1 {"Iris-setosa"} else {"Not Iris-setosa"};
+
         println!("Test data: {:?} {}", &rek.attribs, &rek.class);
         println!("Classified as: {}", &res);
         count += 1;
-        if res.eq(&rek.class) {
+        if (out == 1 && res.eq(&rek.class)) || (out == 0 && rek.class.ne("Iris-setosa")) {
             hit += 1;
         }
     }
     println!("Test count: {}\nHit count: {}\nPercentage: {:.2}%", count, hit, hit as f64 / count as f64 * 100.0); 
-
     println!("Insert vector to classify: [attrb1 <whitespace> attrb2 <whitespace> ..] or 'q' to end");
     buffer.clear();
     let _ = stdin.read_line(&mut buffer);
     while !buffer.trim().eq("q") {
         let rek = line_to_rekord(buffer.trim(), true);
-        println!("Testing: {:?}", rek.attribs);
-        println!("Classified as: {}", train_data.classify(k, &rek.attribs));
+        let out = perceptron.get_output(&rek);
+        let res = if out == 1 {"Iris-setosa"} else {"Not Iris-setosa"};
+        println!("Testing: {:?}", &rek.attribs);
+        println!("Classified as: {}", res);
 
         println!("Insert vector to classify: [attrb1 <whitespace> attrb2 <whitespace> ..] or 'q' to end");
         buffer.clear();
@@ -55,14 +64,27 @@ fn main() {
     }
 }
 
-fn from_file(path: &str) -> TrainData {
+fn from_file(path: &str, alpha: f64) -> Perceptron {
     let contents = fs::read_to_string(path)
         .expect("Failed to read file");
     let mut records: Vec<Rekord> = Vec::new();
     for line in contents.lines() {
         records.push(line_to_rekord(line, false));
     }
-    TrainData { records }
+    let mut perc = Perceptron {
+        weights: vec![0.0; records[0].attribs.len()],
+        theta: 0.0,
+    };
+    records.shuffle(&mut thread_rng());
+    for rek in records {
+        let d = if rek.class == "Iris-setosa" {1} else {0};
+        let y = perc.get_output(&rek);
+        for i in 0..perc.weights.len() {
+            perc.weights[i] += (d - y) as f64 * alpha * rek.attribs.clone()[i];
+        }
+        perc.theta += (d - y) as f64 * alpha * -1.0;
+    }
+    perc
 }
 
 fn line_to_rekord(line: &str, noclass: bool) -> Rekord {
@@ -88,40 +110,14 @@ fn line_to_rekord(line: &str, noclass: bool) -> Rekord {
         class: cls,
     }
 }
-
-impl TrainData {
-    fn classify(&self, k: i32, in_vec: &Vec<f64>) -> String {
-        let mut rip_mem: Vec<(f64, String)> = Vec::new();
-        for rek in self.records.iter() {
-            let dist = rek.distance(in_vec);
-            rip_mem.push((dist, rek.class.clone()));
+impl Perceptron {
+    fn get_output(&self, rek: &Rekord) -> i32 {
+        let mut w_val = 0.0;
+        for z_iter in self.weights.iter().zip(rek.attribs.iter()) {
+            w_val += z_iter.0 * z_iter.1;
         }
-        rip_mem.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-        let mut best: HashMap<&String, i32> = HashMap::new();
-        for it in (0..k).zip(rip_mem.iter()) {
-            *best.entry(&it.1.1).or_insert(0) += 1;
-        }
-        let mut ret: String = String::new();
-        let mut max: i32 = 0;
-        for key in best.keys() {
-            if best[key] > max {
-                max = best[key];
-                ret = key.to_string();
-            }
-        }
-        ret
-    }
-}
-
-impl Rekord {
-    fn distance(&self, other: &Vec<f64>) -> f64 {
-        let mut sum = 0.;
-        for it in self.attribs.iter().zip(other.iter()) {
-            let (s, o) = it;
-            sum += (s - o).powi(2);
-        }
-        //sum.sqrt() ommitted
-        sum
+        //println!("perc: {:?}\nvec: {:?}\nval: {} theta: {}", self.weights, rek.attribs, w_val, self.theta);
+        if w_val >= self.theta {1} else {0}
     }
 }
 
